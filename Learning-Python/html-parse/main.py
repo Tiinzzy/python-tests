0baef0a59ffc4319fd6bd1938a6531f05234905c
Tina <tinzzy.cracked@yahoo.com>
2023-02-09 19:18:02 -0500

    send data to node.js

---
+++ Learning-Python/html-parse/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from phil import get_all_data
+
+app = Flask(__name__)
+
+
+@app.route("/all-relationships")
+def all_relationships():
+    data = get_all_data()
+    return jsonify(data)
+
+
+# if __name__ == "__main__":
+#     data = get_all_data()
+#     print(data)
