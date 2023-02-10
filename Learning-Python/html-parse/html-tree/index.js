3e781d914f8026ea5b5e8e099c9134935e838082
Tina <tinzzy.cracked@yahoo.com>
2023-02-09 19:17:51 -0500

    run node command to create graphiz svg

---
+++ Learning-Python/html-parse/html-tree/index.js
@@ -0,0 +1,32 @@
+const BackEndConnection = require('./BackEndConnection');
+const backend = BackEndConnection.INSTANCE();
+
+var util = require('util');
+var graphviz = require('graphviz');
+
+
+backend.get_wikipedias_relationships((data) => {
+  displayGraphvis(data);
+});
+
+
+function displayGraphvis(relations) {
+  var graph = graphviz.digraph("G");
+
+  relations.map(r => r.from_title).forEach(n => {
+    graph.addNode(n);
+  });
+
+  relations.forEach(r => {
+    let fromTitle = r.from_title;
+    let toTitle = r.to_title;
+    graph.addEdge(fromTitle, toTitle, { "minlen": 2 });
+  });;
+
+  
+  graph.setGraphVizPath("/usr/bin");
+  graph.output("svg", "/home/tina/Downloads/html-tree.svg");
+
+  console.log('-----------------------------');
+  console.log(graph.to_dot());
+}
