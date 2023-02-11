4c792a9abcfc857de4537807bdc5b2165f93c365
Tina <tinzzy.cracked@yahoo.com>
2023-02-11 11:49:34 -0500

    trying to create a new approach to search for html tags and their attributes in a cleaner way and crawl the web with it.

---
+++ Learning-Python/html-parse/web-crawler-1.py
@@ -0,0 +1,103 @@
+import sys
+import re
+from database import Database
+import ssl
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.parse
+import urllib.error
+import collections
+collections.Callable = collections.abc.Callable
+
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+SECION_TAG = 'section'
+SECION_CLASSES = ['module', 'module--news', 'module--collapse-images']
+
+NEWS_TAG = 'div'
+NEWS_CLASSES = ['module__content']
+
+
+ANCHOR_TAG = 'a'
+
+
+def get_page(url):
+    try:
+        html = urllib.request.urlopen(start_link, context=ctx).read()
+        soup = BeautifulSoup(html, 'lxml')
+        return html, soup
+    except:
+        return None, None
+
+
+def is_a_tag(element, tag_id):
+    is_tag = (element.name == tag_id)
+    return is_tag
+
+
+def tag_has_classes(element, classes):
+    has_classes = True
+    for cls in classes:
+        if 'class' in element.attrs.keys():
+            element_classes = element.attrs['class']
+            if cls not in element_classes:
+                has_classes = False
+        else:
+            has_classes = False
+
+    return has_classes
+
+
+def is_a_tag_with_classes(element, tag, classes):
+    if is_a_tag(element, tag):
+        if tag_has_classes(element, classes):
+            return True
+    return False
+
+
+def test_1(start_link):
+    html, soup = get_page(start_link)
+
+    section_found = False
+    news_found = False
+    for chld in soup.recursiveChildGenerator():
+        if not news_found:
+            if not section_found and is_a_tag_with_classes(chld, SECION_TAG, SECION_CLASSES):
+                section_found = True
+            if section_found and is_a_tag_with_classes(chld, NEWS_TAG, NEWS_CLASSES):
+                news_found = True
+                print('------------------------>\n')
+                print(chld)
+                print('\n<------------------------')
+
+
+def get_anchor_href(element):
+    if 'href' in element.attrs.keys():
+        return element.attrs['href']
+    else:
+        return None
+
+
+def get_url_hrefs(url):
+    urls = []
+    html, soup = get_page(url)
+    for chld in soup.recursiveChildGenerator():
+        if is_a_tag(chld, ANCHOR_TAG):
+            href = get_anchor_href(chld)
+            if href is not None:
+                urls.append(href)
+    return urls
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        start_link = sys.argv[1]
+    else:
+        # start_link = input('Please enter a wikipedia link: ')
+        start_link = 'https://www.bbc.com/'
+
+    urls = get_url_hrefs(start_link)
+    print(urls)
