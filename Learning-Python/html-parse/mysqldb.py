bbb76b28aaf3f1e8d93be12272be7b83dd1e8b0d
Tina <tinzzy.cracked@yahoo.com>
2023-02-10 19:08:23 -0500

    code cleanup

--- Learning-Python/html-parse/mysqldb.py
+++ Learning-Python/html-parse/mysqldb.py
@@ -4,13 +4,8 @@ import pandas as pd
 my_conn = mysql.connector.connect(
     host="127.0.0.1", database='tests', user="dbadmin", passwd="washywashy")
 
-
-
-
 query = "Select * from tests.wiki_articles_relations;"
 df = pd.read_sql(query, my_conn)
-
-
 
 for index, row in df.iterrows():
     print(index, ' , ', row['from_title'], ' , ', row['to_title'])
