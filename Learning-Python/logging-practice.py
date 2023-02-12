1b7bc02e03fe252c52d0e1659b7c87ccb095a3c3
Tina <tinzzy.cracked@yahoo.com>
2023-02-12 17:47:18 -0500

    tried sending email to others through smtp

--- Learning-Python/logging-practice.py
+++ Learning-Python/logging-practice.py
@@ -1,9 +1,14 @@
 import logging
+import smtplib
+from email.mime.text import MIMEText
+
+
+SENDER_EMAIL = "senders email"
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s|%(levelname)s|%(message)s',
                     datefmt="%Y-%m-%d %H:%M:%S",
-                    filename="/home/tina/Downloads/logs/my-pyton-practice.log", 
+                    filename="/home/tina/Downloads/logs/my-pyton-practice.log",
                     filemode="a")
 
 
@@ -11,9 +16,33 @@ def process_url(url):
     logging.error(f"someting gone wrong during fetching data from URL: {url}")
 
 
-if __name__ == "__main__":
+def send_email(subject, body, to_address):
+    logging.info(f"sending email to {to_address}")
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = SENDER_EMAIL
+    msg['To'] = to_address
+    try:
+        smtpObj = smtplib.SMTP_SSL('senders email', 465)
+        smtpObj.ehlo()
+        smtpObj.login('your own username!', 'password')
+        smtpObj.sendmail(SENDER_EMAIL, to_address, msg.as_string())
+        smtpObj.close()
+        print(msg)
+        logging.info(f"email sent successfully")
+    except:
+        logging.info(f"something gone wrong while sending email!")
+
+
+def test1():
     logging.info(f"Just started the process")
-    url = 'https://www.cnn.com'    
+    url = 'https://www.cnn.com'
     process_url(url)
     logging.info(f"Process ended successfully!")
-    
+
+
+if __name__ == "__main__":
+    # test1()
+
+    send_email("PAY US 100$ AND SUCCESS WILL BE YOURS!",
+               "Hi there, ...", "recievers email")
