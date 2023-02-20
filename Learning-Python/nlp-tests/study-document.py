9eff4ea9b5cfc8c36e92454f16dc5c80b8568f57
Tina <tinzzy.cracked@yahoo.com>
2023-02-20 10:20:10 -0500

    method to tokenize a given text

--- Learning-Python/nlp-tests/study-document.py
+++ Learning-Python/nlp-tests/study-document.py
@@ -1,3 +1,4 @@
+from nltk.tokenize import TweetTokenizer
 import ssl
 from bs4 import BeautifulSoup
 import urllib.request
@@ -13,13 +14,13 @@ class Nltk_Process:
         self.ctx.check_hostname = False
         self.ctx.verify_mode = ssl.CERT_NONE
 
-    def open_and_read_text(self, file_name):
+    def open_file_and_read_text(self, file_name):
         text_file = open(
             '/home/tina/Documents/python/python-tests/Learning-Python/nlp-tests/' + file_name, 'r')
         text = text_file.read()
         return text
 
-    def open_and_read_url(self, url):
+    def __open_and_read_url(self, url):
         try:
             html = urllib.request.urlopen(url, context=self.ctx).read()
             soup = BeautifulSoup(html, 'lxml')
@@ -27,17 +28,24 @@ class Nltk_Process:
         except:
             return None
 
-    def read_web_page(self, url):
-        soup = self.open_and_read_url(url)
+    def read_web_page_text(self, url):
+        soup = self.__open_and_read_url(url)
         text = soup.get_text()
         return text
+
+    def tokenize_text(self, text):
+        tokens = TweetTokenizer().tokenize(text)
+        return tokens
 
 
 if __name__ == "__main__":
     test = Nltk_Process()
-    text = test.open_and_read_text('text.txt')
+    text = test.open_file_and_read_text('text.txt')
     print(text)
-    print('\n' * 5)
-    text = test.read_web_page(
-        'https://www.foodnetwork.ca/recipe/air-fryer-cauliflower-bites/')
-    print(text)
+    # print('\n' * 5)
+    # text = test.read_web_page_text(
+    #     'https://www.foodnetwork.ca/recipe/air-fryer-cauliflower-bites/')
+    # print(text)
+
+    token = test.tokenize_text(text)
+    print(token)
