71cad5295348174bf11d91af95ccbf7fc0a96a89
Tina <tinzzy.cracked@yahoo.com>
2023-02-21 15:53:39 -0500

    code cleanup and fixing display of tokens on clicking button

--- Learning-Python/nlp-tests/nltk/src/components/frontend/TextTokens.js
+++ Learning-Python/nlp-tests/nltk/src/components/frontend/TextTokens.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import TextField from '@mui/material/TextField';
 import Box from '@mui/system/Box';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
@@ -12,13 +11,14 @@ export default class TextTokens extends React.Component {
         super(props);
         this.state = {
             data: props.data,
-            tokens: []
+            tokens: [],
+            cleanTokens: []
         };
 
     }
 
     getText() {
-        this.setState({ text: this.state.data.text });
+        this.setState({ text: this.state.data.text, tokens: [] });
     }
 
     getTokens() {
@@ -26,27 +26,40 @@ export default class TextTokens extends React.Component {
         for (let i in this.state.data.tokens) {
             tokens.push(i + ' --> ' + this.state.data.tokens[i]);
         }
-        this.setState({ tokens: tokens });
-        console.log(tokens);
+        this.setState({ tokens: tokens, text: null, cleanTokens: [] });
     }
 
     getCleanTokens() {
-        this.setState({ cleanTokens: this.state.data.nonStopWord });
-        console.log(this.state.cleanTokens);
+        let cleanTokens = [];
+        for (let i in this.state.data.nonStopWord) {
+            cleanTokens.push(i + ' --> ' + this.state.data.nonStopWord[i]);
+        }
+        this.setState({ cleanTokens: cleanTokens, text: null, tokens: [] });
     }
 
     render() {
         return (
             <Box className="MainTextTokenBox">
                 <Divider style={{ marginBottom: 20 }} color="#4c4c4c" />
-                <Button variant="contained" className='GetTokensBtn' size='large' onClick={() => this.getText()}>Text</Button>
-                <Button variant="contained" className='GetTokensBtn' size='large' onClick={() => this.getTokens()} >Tokens</Button>
-                <Button variant="contained" className='GetTokensBtn' size='large' onClick={() => this.getCleanTokens()} >CLean Tokens</Button>
+                <Button variant="contained" className='GetTokensBtn' size='medium' onClick={() => this.getText()}>Text</Button>
+                <Button variant="contained" className='GetTokensBtn' size='medium' onClick={() => this.getTokens()} >Tokens</Button>
+                <Button variant="contained" className='GetTokensBtn' size='medium' onClick={() => this.getCleanTokens()} >CLean Tokens</Button>
 
                 {this.state.text !== null && <Box className="DataDisplayBox">{this.state.text}</Box>}
-                {/* {this.state.tokens && <Box> {this.state.tokens.forEach((e, i) => {
-                    <Box key={i}> {e}</Box>
-                })}</Box>} */}
+
+                {this.state.tokens.length > 0 &&
+                    <Box className="DataDisplayBox">
+                        {this.state.tokens.map((e, i) => (
+                            <div key={i}>{e}</div>
+                        ))}
+                    </Box>}
+
+                {this.state.cleanTokens.length > 0 &&
+                    <Box className="DataDisplayBox">
+                        {this.state.cleanTokens.map((e, i) => (
+                            <div key={i}>{e}</div>
+                        ))}
+                    </Box>}
             </Box>
         );
     }
