61e9f72eab1fb6a2e07c8f01bf936e8da98d3301
Tina <tinzzy.cracked@yahoo.com>
2023-02-21 14:51:18 -0500

    clearing up as changing the button, getting different  data based on different button

--- Learning-Python/nlp-tests/nltk/src/components/frontend/TextTokens.js
+++ Learning-Python/nlp-tests/nltk/src/components/frontend/TextTokens.js
@@ -17,31 +17,35 @@ export default class TextTokens extends React.Component {
     }
 
     getText() {
-        this.setState({ text: this.state.data.text, tokens: '' });
+        this.setState({ text: this.state.data.text });
     }
 
     getTokens() {
-        this.setState({ text: '' });
         let tokens = [];
         for (let i in this.state.data.tokens) {
             tokens.push(i + ' --> ' + this.state.data.tokens[i]);
-            let tokenBox = document.getElementById('display-tokens');
-            tokenBox.scrollTop = tokenBox.scrollHeight;
         }
         this.setState({ tokens: tokens })
+        console.log(tokens)
+    }
+
+    getCleanTokens() {
+        this.setState({ cleanTokens: this.state.data.nonStopWord });
     }
 
     render() {
         return (
             <Box style={{ marginTop: 20 }}>
                 <Divider style={{ marginBottom: 20 }} color="#696969" />
+                <Button variant="contained" size='large' onClick={() => this.getText()} style={{ marginRight: 15 }}>Text</Button>
                 <Button variant="contained" size='large' onClick={() => this.getTokens()} style={{ marginRight: 15 }}>Tokens</Button>
-                <Button variant="contained" size='large' onClick={() => this.getText()}>Text</Button>
+                <Button variant="contained" size='large' onClick={() => this.getCleanTokens()} style={{ marginRight: 15 }}>CLean Tokens</Button>
+
                 {this.state.text !== null &&
                     <Box style={{ marginTop: 20, overflowY: 'scroll', height: 600, width: 600 }}>{this.state.text}</Box>}
-                {this.state.tokens && this.state.tokens.map((e, i) => {
-                    <div key={i} id="display-tokens" style={{ overflowY: 'scroll', height: 600, width: 600 }}>{e}</div>
-                })}
+                {this.state.tokens && <Box> {this.state.tokens.forEach((e, i) => {
+                    <Box key={i}> {e}</Box>
+                })}</Box>}
             </Box>
         );
     }
