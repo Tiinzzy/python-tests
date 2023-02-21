0e69520faff5ca7c481c1e5a197f48958574520a
Tina <tinzzy.cracked@yahoo.com>
2023-02-21 13:49:41 -0500

    added component and some changes to set data that getting from backend to send to the display component

--- Learning-Python/nlp-tests/nltk/src/components/frontend/Url.js
+++ Learning-Python/nlp-tests/nltk/src/components/frontend/Url.js
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import { Base64 } from 'js-base64';
 
 import BackEndConnection from './BackEndConnection';
+import TextTokens from './TextTokens';
 
 const backend = BackEndConnection.INSTANCE();
 
@@ -16,35 +17,37 @@ export default class Url extends React.Component {
         super(props);
         this.state = {
             url: null,
-            value: 'https://www.cnn.com/'
+            value: 'https://www.bbc.com/'
         };
 
     }
 
-    GetTextfieldValue(e) {
+    getTextfieldValue(e) {
         this.setState({ url: e.target.value })
     }
 
-    SubmitUrl() {
+    submitUrl() {
         let url = Base64.encode(this.state.value);
-        console.log(url, '<<<<<<<')
+        let that = this;
         backend.send_url_to_backend(url, (data) => {
-            console.log(data)
+            that.setState({ data: data });
         })
     }
 
     render() {
         return (
-            <>
+            <Box>
                 <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: 50 }}>
                     <Typography mb={1} fontSize={18} variant="body1">Enter a Url</Typography>
-                    <TextField value={this.state.value} variant="outlined" onChange={(e) => this.GetTextfieldValue(e)} />
+                    <TextField value={this.state.value} variant="outlined" onChange={(e) => this.getTextfieldValue(e)} />
 
                 </Box >
                 <Box style={{ justifyContent: 'right', display: 'flex', marginTop: 20 }}>
-                    <Button variant="contained" size='large' onClick={() => this.SubmitUrl()}>Submit</Button>
+                    <Button variant="contained" size='large' onClick={() => this.submitUrl()}>Submit</Button>
                 </Box>
-            </>
+                {this.state.data &&
+                    <TextTokens data={this.state.data} />}
+            </Box>
         );
     }
 };
