b08c4a7f3583a673eb48abb9ecb9059b53f5d661
Tina <tinzzy.cracked@yahoo.com>
2023-02-21 14:46:58 -0500

    sending non stop word tokens to frontend

--- Learning-Python/nlp-tests/nltk/src/components/backend/main.py
+++ Learning-Python/nlp-tests/nltk/src/components/backend/main.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 def top_ten_movies():
     args = request.args
     url = args.get('url')
-    print(url, '<<<<<<<<<<<<<')
     main_url = base64.b64decode(url).decode('utf-8')
-    print('>>>>>>>>>>>>>>>>>>>>>>', main_url)
 
     NltkProcess.init_web_page(main_url).tokenize().remove_stop_words()
 
     text = NltkProcess.get_text()
     tokens = NltkProcess.get_tokens()
+    nonStopWord = NltkProcess.get_no_stop_words_tokens()
 
-    return jsonify({'text': text, 'tokens': tokens})
+    result = {'text': text, 'tokens': tokens, 'nonStopWord': nonStopWord}
+
+    return jsonify(result)
