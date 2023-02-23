b78b0211e9691e65c7425ca65f0fe45cb59fd52a
Tina <tinzzy.cracked@yahoo.com>
2023-02-23 18:38:41 -0500

    same components as before, instead of duplicating, created a key to distinguish different data and display accordingly

---
+++ Learning-Python/nlp-tests/nltk/src/components/frontend/GenericTokenForText.js
@@ -0,0 +1,191 @@
+import React from 'react';
+
+import Box from '@mui/system/Box';
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+
+import CommonWordsFrequency from './CommonWordsFrequency';
+import { shared } from './helper';
+
+import './style.css';
+
+export default class GenericTokenForText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            service: props.service,
+            data: props.data,
+            tokens: [],
+            cleanTokens: [],
+            frequency: null,
+            commonWords: null
+        };
+        this.callTokenForText = this.callTokenForText.bind(this);
+        shared.callTokenForText = this.callTokenForText;
+    }
+
+    getText() {
+        let text = (this.state.service === 'text' ? this.state.data.text.text : this.state.data.text);
+        this.setState({ text, tokens: [], commonWords: null, cleanTokens: [], frequency: null });
+    }
+
+    getTokens() {
+        let tokens = [];
+        for (let i in this.state.data.tokens) {
+            tokens[i] = [i, this.state.data.tokens[i]];
+        }
+        this.setState({ tokens: tokens, text: null, cleanTokens: [], commonWords: null, frequency: null });
+    }
+
+    getCleanTokensUrl() {
+        let cleanTokens = [];
+        for (let i in this.state.data.nonStopWord) {
+            cleanTokens[i] = [i, this.state.data.nonStopWord[i]];
+        }
+        this.setState({ cleanTokens: cleanTokens, text: null, tokens: [], commonWords: null, frequency: null });
+    }
+
+    getCleanTokensText() {
+        let cleanTokens = [];
+        for (let i in this.state.data.clean) {
+            cleanTokens[i] = [i, this.state.data.clean[i]];
+        }
+        this.setState({ cleanTokens: cleanTokens, text: null, tokens: [], commonWords: null, frequency: null });
+    }
+
+    getCleanTokens() {
+        if (this.state.service === 'text') {
+            this.getCleanTokensText()
+        } else if (this.state.service === 'url') {
+            this.getCleanTokensUrl()
+        }
+    }
+
+    callTokenForText(e) {
+        if (e.action === 'get-common-words-for-text') {
+            let common_words = [];
+            let info = e.data.common_words;
+            for (let i in info) {
+                common_words[i] = [info[i][0], info[i][1]];
+            }
+            this.setState({ commonWords: common_words, text: null, cleanTokens: [], tokens: [], frequency: null });
+        } else if (e.action === 'get-common-words-for-url') {
+            let common_words = [];
+            let info = e.data.common_words;
+            for (let i in info) {
+                common_words[i] = [info[i][0], info[i][1]];
+            }
+            this.setState({ commonWords: common_words, text: null, cleanTokens: [], tokens: [], frequency: null })
+        } else if (e.action === 'get-frequency-words-for-text') {
+            this.setState({ frequency: e.data.freq, text: null, cleanTokens: [], tokens: [], commonWords: null })
+        } else if (e.action === 'get-frequency-words-for-url') {
+            this.setState({ frequency: e.data.freq, text: null, cleanTokens: [], tokens: [], commonWords: null })
+        }
+    }
+
+    render() {
+        return (
+            <Box className="MainTextTokenBox">
+                <Divider style={{ marginBottom: 20 }} color="#4c4c4c" />
+                <Box display='flex'>
+                    <Button variant="contained" className='GetTokensBtn' size='medium' onClick={() => this.getText()}>Text</Button>
+                    <Button variant="contained" className='GetTokensBtn' size='medium' onClick={() => this.getTokens()} >Tokens</Button>
+                    <Button variant="contained" className='GetTokensBtn' size='medium' onClick={() => this.getCleanTokens()} >CLean Tokens</Button>
+                    <CommonWordsFrequency />
+                </Box>
+
+                {this.state.text !== null && <Box className="DataDisplayBox">{this.state.text}</Box>}
+
+                {this.state.tokens.length > 0 &&
+                    <Box className="DataDisplayBox">
+                        <table width="100%">
+                            <tbody >
+                                <tr>
+                                    <th>index</th>
+                                    <th>Tokenized Word</th>
+                                </tr>
+                                {this.state.tokens.map((e, i) => (
+                                    <tr key={i} >
+                                        <td>
+                                            {e[0] * 1 + 1}
+                                        </td>
+                                        <td>
+                                            {e[1]}
+                                        </td>
+                                    </tr>))}
+                            </tbody>
+                        </table>
+                    </Box>}
+
+                {this.state.cleanTokens.length > 0 &&
+                    <Box className="DataDisplayBox">
+                        <table width="100%">
+                            <tbody >
+                                <tr>
+                                    <th>index</th>
+                                    <th>Clean Tokenized Word</th>
+                                </tr>
+                                {this.state.cleanTokens.map((e, i) => (
+                                    <tr key={i} >
+                                        <td>
+                                            {e[0] * 1 + 1}
+                                        </td>
+                                        <td>
+                                            {e[1]}
+                                        </td>
+                                    </tr>))}
+                            </tbody>
+                        </table>
+                    </Box>}
+                {this.state.commonWords !== null &&
+                    <Box className="DataDisplayBox">
+                        <table width="100%">
+                            <tbody >
+                                <tr>
+                                    <th>index</th>
+                                    <th>Word</th>
+                                    <th>Count</th>
+                                </tr>
+                                {this.state.commonWords.map((e, i) => (
+                                    <tr key={i} >
+                                        <td>
+                                            {i + 1}
+                                        </td>
+                                        <td>
+                                            {e[0]}
+                                        </td>
+                                        <td>
+                                            {e[1]}
+                                        </td>
+                                    </tr>))}
+                            </tbody>
+                        </table>
+                    </Box>}
+                {this.state.frequency !== null &&
+                    <Box className="DataDisplayBox">
+                        <table width="100%">
+                            <tbody >
+                                <tr>
+                                    <th>index</th>
+                                    <th>Word</th>
+                                    <th>Frequency</th>
+                                </tr>
+                                {this.state.frequency.map((e, i) => (
+                                    <tr key={i} >
+                                        <td>
+                                            {i + 1}
+                                        </td>
+                                        <td>
+                                            {e[0]}
+                                        </td>
+                                        <td>
+                                            {(e[1] * 1).toFixed(4)}
+                                        </td>
+                                    </tr>))}
+                            </tbody>
+                        </table>
+                    </Box>}
+            </Box>
+        );
+    }
+};
