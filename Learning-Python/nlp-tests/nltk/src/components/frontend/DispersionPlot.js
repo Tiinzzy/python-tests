ad735194f273827a77ab2e379a84fc836177b5b3
Tina <tinzzy.cracked@yahoo.com>
2023-02-24 13:53:33 -0500

    fixed display of data in the dialog and getting data ready for submit  by adding into array

--- Learning-Python/nlp-tests/nltk/src/components/frontend/DispersionPlot.js
+++ Learning-Python/nlp-tests/nltk/src/components/frontend/DispersionPlot.js
@@ -1,22 +1,54 @@
 import React from 'react';
 
 import Box from '@mui/system/Box';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
 
 import './style.css';
+
+const SELECTED_WORDS = [];
 
 export default class DispersionPlot extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            allWords: props.allWords,
+            close: props.close
         };
     }
 
+    componentDidMount() {
+        let uniqWOrds = [...new Set(this.state.allWords)];
+        this.setState({ uniqWOrds })
+    }
 
+    wordSelected(e) {
+        SELECTED_WORDS.push(e.target.innerText);
+    }
+
+    submitWords() {
+        this.state.close();
+        console.log(SELECTED_WORDS);
+    }
 
     render() {
         return (
             <Box>
-                hello
+                <DialogTitle>
+                    {"Select the words you would like to compare the frequency across the text:"}
+                </DialogTitle>
+                <DialogContent>
+                    <div className='EachWordDiv'>
+                        {this.state.uniqWOrds && this.state.uniqWOrds.map((e, i) => (
+                            <div key={i} className="EachWord" onClick={(e) => this.wordSelected(e)}>{e}</div>
+                        ))}
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => this.submitWords()}>Submit</Button>
+                </DialogActions>
             </Box>
         );
     }
