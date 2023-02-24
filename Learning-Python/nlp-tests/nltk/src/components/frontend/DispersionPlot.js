6965b2080f5adad90fab3b264093c14a032eef43
Tina <tinzzy.cracked@yahoo.com>
2023-02-24 11:27:23 -0500

    component to create dispersion plot

---
+++ Learning-Python/nlp-tests/nltk/src/components/frontend/DispersionPlot.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Box from '@mui/system/Box';
+
+import './style.css';
+
+export default class DispersionPlot extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+
+
+    render() {
+        return (
+            <Box>
+                hello
+            </Box>
+        );
+    }
+};
