349bdef3b80f6755d79d5afd7c1760807bc132cf
Tina <tinzzy.cracked@yahoo.com>
2023-02-24 11:25:50 -0500

    added method to create lexical dispersion plot

--- Learning-Python/nlp-tests/nltk/src/components/backend/nlp.py
+++ Learning-Python/nlp-tests/nltk/src/components/backend/nlp.py
@@ -74,6 +74,31 @@ class NltkProcess:
         return df
 
     @classmethod
+    def draw_lexical_dispersion_plot(self, words, ignore_case=False, title="Lexical Dispersion Plot"):
+        word2y = {
+            word.casefold() if ignore_case else word: y
+            for y, word in enumerate(reversed(words))
+        }
+        xs, ys = [], []
+        for x, token in enumerate(self.tokens):
+            token = token.casefold() if ignore_case else token
+            y = word2y.get(token)
+            if y is not None:
+                xs.append(x)
+                ys.append(y)
+
+        _, ax = plt.subplots()
+        ax.plot(xs, ys, "|")
+        ax.set_yticks(list(range(len(words))), words, color="C0")
+        ax.set_ylim(-1, len(words))
+        ax.set_title(title)
+        ax.set_xlabel("Word Offset")
+        ax.set_xlim([0, len(self.tokens)])
+
+        plt.show()
+        return ax
+
+    @classmethod
     def get_text(self):
         for i in self.text:
             txt = ' '.join(self.text.split())
@@ -86,7 +111,6 @@ class NltkProcess:
     @classmethod
     def get_no_stop_words_tokens(self):
         return self.no_stop_word_tokens
-
 
 
 if __name__ == "__main__":
@@ -106,3 +130,4 @@ if __name__ == "__main__":
     # print('\n' * 10)
     # print(NltkProcess.get_frequency_as_data_frame().head(5).values.tolist())
 
+    NltkProcess.draw_lexical_dispersion_plot(["cnn", "news", "newsletters"])
