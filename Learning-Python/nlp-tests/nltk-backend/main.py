b33a3d175e6cd60432a18e25ed8ff94895f720bd
Tina <tinzzy.cracked@yahoo.com>
2023-02-25 10:58:47 -0500

    fixed all the apis to become stateless while handling data processing

--- Learning-Python/nlp-tests/nltk-backend/main.py
+++ Learning-Python/nlp-tests/nltk-backend/main.py
@@ -9,7 +9,22 @@ import base64
 
 app = Flask(__name__)
 
-nltk_processor = NltkProcess()
+
+def get_nltk_processor(request):
+    text = request.json['text']
+    url = request.json['url']
+    my_nltk_processor = NltkProcess()
+
+    if url is not None:
+        main_url = base64.b64decode(url).decode('utf-8')
+        my_nltk_processor.init_web_page(
+            main_url).tokenize().remove_stop_words()
+    else:
+        my_nltk_processor.tokenize(text).remove_stop_words()
+
+    my_nltk_processor.get_tokens()
+    my_nltk_processor.get_no_stop_words_tokens()
+    return my_nltk_processor
 
 
 @app.route("/url-to-txt-and-tokens", methods=['GET'])
@@ -18,11 +33,11 @@ def process_url():
     url = args.get('url')
     main_url = base64.b64decode(url).decode('utf-8')
 
-    nltk_processor.init_web_page(main_url).tokenize().remove_stop_words()
-
-    text = nltk_processor.get_text()
-    tokens = nltk_processor.get_tokens()
-    nonStopWord = nltk_processor.get_no_stop_words_tokens()
+    my_nltk_processor = NltkProcess()
+    my_nltk_processor.init_web_page(main_url).tokenize().remove_stop_words()
+    text = my_nltk_processor.get_text()
+    tokens = my_nltk_processor.get_tokens()
+    nonStopWord = my_nltk_processor.get_no_stop_words_tokens()
 
     result = {'text': text, 'tokens': tokens, 'nonStopWord': nonStopWord}
 
@@ -33,58 +48,67 @@ def process_url():
 def process_text_file():
     text = request.json['text']
 
-    nltk_processor.tokenize(text).remove_stop_words()
-
-    tokens = nltk_processor.get_tokens()
-    no_stopwords = nltk_processor.get_no_stop_words_tokens()
+    my_nltk_processor = NltkProcess()
+    my_nltk_processor.tokenize(text).remove_stop_words()
+    tokens = my_nltk_processor.get_tokens()
+    no_stopwords = my_nltk_processor.get_no_stop_words_tokens()
 
     result = {'tokens': tokens, 'no_stopwords': no_stopwords}
     return jsonify(result)
 
 
-@app.route("/get-common-words-count", methods=['GET'])
+@app.route("/get-common-words-count", methods=['POST'])
 def get_common_words():
-    args = request.args
-    count = args.get('count')
+    count = request.json['count']
     count = int(count)
 
-    common_words = nltk_processor.get_most_common_words(
-        count, nltk_processor.get_no_stop_words_tokens())
-    all_freq = len(nltk_processor.get_frequency_as_data_frame())
+    my_nltk_processor = get_nltk_processor(request)
+    common_words = my_nltk_processor.get_most_common_words(
+        count, my_nltk_processor.get_no_stop_words_tokens())
+    all_freq = len(my_nltk_processor.get_frequency_as_data_frame())
 
     result = {'common_words': common_words, 'all': all_freq}
-
     return jsonify(result)
 
 
-@app.route("/get-frequency-of-words", methods=['GET'])
+@app.route("/get-frequency-of-words", methods=['POST'])
 def get_frequency_of_words():
-    args = request.args
-    count = args.get('count')
+    count = request.json['count']
     count = int(count)
 
-    freq = nltk_processor.get_frequency_as_data_frame()
+    my_nltk_processor = get_nltk_processor(request)
+    freq = my_nltk_processor.get_frequency_as_data_frame()
     frequency = freq.head(count).values.tolist()
     result = {'freq': frequency}
 
     return jsonify(result)
 
 
-@app.route("/get-graph-of-dispersion-plot", methods=['GET'])
+@app.route("/get-graph-of-dispersion-plot", methods=['POST'])
 def get_dispersion_plot():
-    args = request.args
-    words = args.get('words')
+    words = request.json['words']
     words = words.split(',')
-    figure = nltk_processor.draw_lexical_dispersion_plot(words)
-    output = io.BytesIO()
-    FigureCanvas(figure).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
+
+    my_nltk_processor = get_nltk_processor(request)
+    figure, base64_data = my_nltk_processor.draw_lexical_dispersion_plot(words)
+    print(base64_data)
+    return jsonify({'image': base64_data})
+
+
+@app.route("/get-all-tokens-for-graph", methods=['POST'])
+def get_all_tokens_for_graph():
+    my_nltk_processor = get_nltk_processor(request)
+    clean_tokens = my_nltk_processor.get_no_stop_words_tokens()
+    result = {'cleanTokens': clean_tokens}
+    return jsonify(result)
 
 
 @app.route("/get-sample-chart", methods=['GET'])
 def get_sample_chart():
     print('Hi THERE !!!')
-    figure = nltk_processor.get_sample_chart_fig()
+
+    my_nltk_processor = NltkProcess()
+    figure = my_nltk_processor.get_sample_chart_fig()
     output = io.BytesIO()
     FigureCanvas(figure).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
