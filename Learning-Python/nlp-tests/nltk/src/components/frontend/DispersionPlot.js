06bb01f0889473ed1b79564fb1e1d09910ab8462
Tina <tinzzy.cracked@yahoo.com>
2023-02-24 19:23:56 -0500

    fixed checkboxes being slow and getting parameter/ need to fix it later

--- Learning-Python/nlp-tests/nltk/src/components/frontend/DispersionPlot.js
+++ Learning-Python/nlp-tests/nltk/src/components/frontend/DispersionPlot.js
@@ -9,6 +9,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 import BackEndConnection from './BackEndConnection';
+import { shared } from './helper';
 
 import './style.css';
 
@@ -36,24 +37,36 @@ export default class DispersionPlot extends React.Component {
     }
 
     wordSelected(e) {
+        let start = Date.now();
         let selectedWords = this.state.selectedWords;
+        console.log(Object.keys(selectedWords).length);
         selectedWords[e] = !selectedWords[e]
-        this.setState({ selectedWords })
+        this.setState({ selectedWords }, function () {
+            console.log(Date.now() - start);
+        })
     }
 
     submitWords() {
         let SELECTED_WORDS = [];
+        console.log(new Date());
         for (let w in this.state.selectedWords) {
             if (this.state.selectedWords[w]) {
                 SELECTED_WORDS.push(w);
             }
         }
+        console.log(new Date());
 
         this.state.close();
         let words = { 'words': SELECTED_WORDS };
-        backend.get_dispersion_plot_graph(words, (data) => {
-            console.log(data);
-        })
+
+        let url = 'http://localhost:8000/get-graph-of-dispersion-plot?words=' + SELECTED_WORDS.join(',')
+        let image = document.getElementById('chart_place_holder');
+        image.src = url;
+        shared.callTokenForText({ action: 'display-graph' })
+        // window.open(url, '_blank')
+        // backend.get_dispersion_plot_graph(words, (data) => {
+        //     console.log(data);
+        // })
     }
 
     render() {
@@ -64,14 +77,15 @@ export default class DispersionPlot extends React.Component {
                 </DialogTitle>
                 <DialogContent>
                     <div className='EachWordDiv'>
-                        {this.state.uniqWOrds && this.state.uniqWOrds.map((e, i) => (
-                            <FormControlLabel key={i} control={<Checkbox onChange={() => this.wordSelected(e)} />} label={e} />
+                        {this.state.uniqWOrds && this.state.uniqWOrds.sort().map((e, i) => (
+                            <FormControlLabel key={i} control={<Checkbox checked={this.state.selectedWords[e]} onChange={() => this.wordSelected(e)} />} label={e} />
                         ))}
                     </div>
                 </DialogContent>
                 <DialogActions mb={2}>
                     <Button onClick={() => this.submitWords()} variant="contained" className='GetTokensBtn' size='medium'>Submit</Button>
                 </DialogActions>
+
             </Box>
         );
     }
