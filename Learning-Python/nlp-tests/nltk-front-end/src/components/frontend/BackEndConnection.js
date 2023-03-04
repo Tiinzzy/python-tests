39d131353c7b75776c807d229ec3f5f8899238a4
Tina <tinzzy.cracked@yahoo.com>
2023-03-04 17:10:32 -0500

    code cleanup

--- Learning-Python/nlp-tests/nltk-front-end/src/components/frontend/BackEndConnection.js
+++ Learning-Python/nlp-tests/nltk-front-end/src/components/frontend/BackEndConnection.js
@@ -7,7 +7,7 @@ class BackEndConnectionImpl {
     async send_url_to_backend(url, callback) {
         this.#url = url;
         this.#text = null;
-
+        
         return axios.get("/url-to-txt-and-tokens?url=" + url, {})
             .then(function (response) {
                 if (callback) {
