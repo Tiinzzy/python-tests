18c47e3efc3325815b6b8008a7e816ea3f8f06b3
Tina <tinzzy.cracked@yahoo.com>
2023-05-01 10:38:20 -0400

    dissecting data from object and passing to functions

--- Learning-Python/practice-yaml/pump.py
+++ Learning-Python/practice-yaml/pump.py
@@ -1,4 +1,5 @@
 from yaml.loader import SafeLoader
+from transfer import get_mysql_data, insert_in_mongodb
 import yaml
 import sys
 
@@ -6,7 +7,22 @@ import sys
 def process_yaml(yaml_filename):
     with open(yaml_filename) as f:
         data = yaml.load(f, Loader=SafeLoader)
-        print(data)
+
+        mysql_host = data['source']['host']
+        mysql_port = data['source']['port']
+        mysql_schema = data['source']['schema']
+        mysql_table = data['source']['table']
+
+        mongodb_host = data['destination']['host']
+        mongodb_port = data['destination']['port']
+        mongodb_schema = data['destination']['schema']
+        mongodb_collection = data['destination']['collection']
+
+        batch_size = data['pump']['batch_size']
+        batch_error = data['pump']['on_error']
+
+        result = get_mysql_data(mysql_table)
+        insert_in_mongodb(result)
 
 
 if __name__ == '__main__':
