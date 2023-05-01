bbf6aca5f56cc7c1825d9a4bb8ea6f7109695af1
Tina <tinzzy.cracked@yahoo.com>
2023-05-01 13:54:50 -0400

    changed format of connection to not get error

--- Learning-Python/practice-yaml/transfer.py
+++ Learning-Python/practice-yaml/transfer.py
@@ -13,14 +13,15 @@ def insert_in_mongodb(documents, mongo_host, mongo_port, mongo_schema, mongo_col
         print('something went wrong')
 
 
-def get_mysql_data(mysql_table, mysql_host, mysql_port, mysql_user, mysql_password, mysql_schema):
-    mysql = MysqlConnection()
-    con, cur = mysql.open_database(mysql_host, mysql_port, mysql_user, mysql_password, mysql_schema)
+def get_mysql_data(mysql_table, host, port, user, password, database):
+    mysql = MysqlConnection().make_connection(host, port, user, password, database)
+
+    conn = mysql.connect()
     query = "Select * from tests.TBALE_NAME;".replace('TBALE_NAME', mysql_table)
 
-    df_result = pd.read_sql(query, con)
-    # average_above_eight = df_result.loc[df_result['vote_average'] >= 8]
+    df_result = pd.read_sql(query, conn)
     result = df_result.to_dict(orient='records')
-    mysql.close_database()
 
+    conn.close()
+    mysql.dispose()
     return result
