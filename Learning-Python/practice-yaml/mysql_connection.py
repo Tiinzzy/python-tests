c1d3ce7ba470d19fd7f19ba9ea1e4c0cb5bc9b11
Tina <tinzzy.cracked@yahoo.com>
2023-05-01 13:55:01 -0400

    creating connection to mysql database with sqlalchemy

--- Learning-Python/practice-yaml/mysql_connection.py
+++ Learning-Python/practice-yaml/mysql_connection.py
@@ -1,15 +1,9 @@
-import mysql.connector as connection
+import sqlalchemy
 
 
 class MysqlConnection:
 
     @classmethod
-    def open_database(cls, host, port, user, password, database):
-        MysqlConnection.con = connection.connect(
-            user=user, password=password, host=host, database=database, port=port, use_pure=True)
-        return MysqlConnection.con, MysqlConnection.con.cursor()
-
-    @classmethod
-    def close_database(cls):
-        MysqlConnection.con.cursor().close()
-        MysqlConnection.con.close()
+    def make_connection(cls, host, port, user, password, database):
+        conn = sqlalchemy.create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}")
+        return conn
