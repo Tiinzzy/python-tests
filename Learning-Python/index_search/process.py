b746e2ec92a4e25bccbaa4a7df782d2bab325098
Tina <tinzzy.cracked@yahoo.com>
2023-05-03 09:56:05 -0400

    looping through and matching if indexs match add to a list, but have to fix to add all indexes that match

--- Learning-Python/index_search/process.py
+++ Learning-Python/index_search/process.py
@@ -3,4 +3,15 @@ def extract_indexes(data):
     for d in data:
         if d["\ufeffIndex"] not in unique_indexes:
             unique_indexes.append(d["\ufeffIndex"])
-    print(unique_indexes)
+
+    save_in_separate_files(unique_indexes, data)
+
+
+def save_in_separate_files(unique_indexes, data):
+    finalized_data = {}
+    for i in range(len(data)):
+        data[i]["\ufeffIndex"]
+        for index in unique_indexes:
+            if data[i]["\ufeffIndex"] == index:
+                finalized_data[index] = data[i]
+    print(finalized_data)
