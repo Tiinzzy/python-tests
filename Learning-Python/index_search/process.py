db179e69968d11c11cbdb893f9ab853f018744d2
Tina <tinzzy.cracked@yahoo.com>
2023-05-03 11:08:54 -0400

    correctly put all indexes in a list with their own data, need to clean up the process

--- Learning-Python/index_search/process.py
+++ Learning-Python/index_search/process.py
@@ -9,9 +9,38 @@ def extract_indexes(data):
 
 def save_in_separate_files(unique_indexes, data):
     finalized_data = {}
+    assignment = []
+    for index in unique_indexes:
+        finalized_data[index] = []
+
     for i in range(len(data)):
-        data[i]["\ufeffIndex"]
-        for index in unique_indexes:
-            if data[i]["\ufeffIndex"] == index:
-                finalized_data[index] = data[i]
+        row_list = list(data[i].values())
+        if unique_indexes[0] in row_list:
+            finalized_data[unique_indexes[0]].append(data[i])
+        elif unique_indexes[1] in row_list:
+            finalized_data[unique_indexes[1]].append(data[i])
+        elif unique_indexes[2] in row_list:
+            finalized_data[unique_indexes[2]].append(data[i])
+        elif unique_indexes[3] in row_list:
+            finalized_data[unique_indexes[3]].append(data[i])
+        elif unique_indexes[4] in row_list:
+            finalized_data[unique_indexes[4]].append(data[i])
+        elif unique_indexes[5] in row_list:
+            finalized_data[unique_indexes[5]].append(data[i])
+        elif unique_indexes[6] in row_list:
+            finalized_data[unique_indexes[6]].append(data[i])
+        elif unique_indexes[7] in row_list:
+            finalized_data[unique_indexes[7]].append(data[i])
+        elif unique_indexes[8] in row_list:
+            finalized_data[unique_indexes[8]].append(data[i])
+        elif unique_indexes[9] in row_list:
+            finalized_data[unique_indexes[9]].append(data[i])
+        elif unique_indexes[10] in row_list:
+            finalized_data[unique_indexes[10]].append(data[i])
+        elif unique_indexes[11] in row_list:
+            finalized_data[unique_indexes[11]].append(data[i])
+        elif unique_indexes[12] in row_list:
+            finalized_data[unique_indexes[12]].append(data[i])
+        elif unique_indexes[13] in row_list:
+            finalized_data[unique_indexes[13]].append(data[i])
     print(finalized_data)
