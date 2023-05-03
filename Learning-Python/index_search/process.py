ba237cf1db146ec62c5133c993acfdbbf7f7b073
Tina <tinzzy.cracked@yahoo.com>
2023-05-03 09:22:48 -0400

    extracting unique indexes to save them in a dictionary

--- Learning-Python/index_search/process.py
+++ Learning-Python/index_search/process.py
@@ -1,2 +1,6 @@
 def extract_indexes(data):
-    print(len(data))
+    unique_indexes = []
+    for d in data:
+        if d["\ufeffIndex"] not in unique_indexes:
+            unique_indexes.append(d["\ufeffIndex"])
+    print(unique_indexes)
