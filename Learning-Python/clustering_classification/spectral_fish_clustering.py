76a83f6471d980504eb620fb66e2f72d3e0509cb
Tina <tinzzy.cracked@yahoo.com>
2023-05-09 14:52:19 -0400

    testing spectrul clustering but still get negative value for score but much closer to 1

--- Learning-Python/clustering_classification/spectral_fish_clustering.py
+++ Learning-Python/clustering_classification/spectral_fish_clustering.py
@@ -1,1 +1,36 @@
-# https://scikit-learn.org/stable/modules/clustering.html#spectral-clustering
+from sklearn.cluster import SpectralClustering
+from sklearn.metrics import silhouette_score
+import pandas as pd
+
+FEATURES = ['Weight', 'Length1', 'Length2', 'Length3', 'Height', 'Width']
+LABELS = ['Species']
+
+
+def get_data():
+    df = pd.read_csv('./data/fish.csv')
+    return df
+
+
+def run_spectral_clustering(df):
+    X = df[FEATURES]
+    # y = df.Species.unique()
+    clustering = SpectralClustering(n_clusters=2, assign_labels='discretize', random_state=0)
+    clustering.fit(X)
+    score = silhouette_score(X, clustering.labels_)
+    print('score: ', score)
+    labels = clustering.labels_
+    print(labels)
+    return clustering
+
+
+def test_sample(df, clustering):
+    df = df.sample(n=2)
+    X = df[FEATURES]
+    prediction = clustering.predict(X)
+    print(prediction)
+
+
+if __name__ == '__main__':
+    ddf = get_data()
+    model = run_spectral_clustering(ddf)
+    test_sample(ddf, model)
