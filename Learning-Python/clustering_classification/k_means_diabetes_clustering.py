6ca25807fd65778bf32096b5d3534f941d6a1b55
Tina <tinzzy.cracked@yahoo.com>
2023-05-09 13:29:08 -0400

    fixed functions for kmean luster but get negative score

--- Learning-Python/clustering_classification/k_means_diabetes_clustering.py
+++ Learning-Python/clustering_classification/k_means_diabetes_clustering.py
@@ -5,8 +5,12 @@ FEATURES = ['gender', 'age', 'hypertension', 'heart_disease', 'smoking_history',
             'blood_glucose_level']
 
 
-def test():
+def get_data():
     df = pd.read_csv('./data/diabetes_prediction_dataset.csv')
+    return df
+
+
+def clean_data(df):
     gender_to_num = {'Female': -1, 'Other': 0, 'Male': 1}
     df.gender = df.gender.apply(lambda x: gender_to_num[x])
 
@@ -19,32 +23,30 @@ def test():
     new_neg_df = neg_df.sample(n=len(pdf))
     new_data_frame = pdf.append(new_neg_df)
     final_data_frame = new_data_frame.sample(frac=1)
+    return final_data_frame
 
-    X = final_data_frame[FEATURES].values
 
-    # Define the number of clusters you want to use
+def run_k_means_clustering(df):
+    X = df[FEATURES].values
     num_clusters = 3
-
-    # Create a KMeans object with the desired number of clusters
     kmeans = KMeans(n_clusters=num_clusters)
-
-    # Fit the KMeans model to the data
     kmeans.fit(X)
-
-    # Extract the cluster labels for each data point
+    score = kmeans.score(X)
+    print('score: ', score)
     labels = kmeans.labels_
-
-    # Print the cluster labels for each data point
     print(labels)
+    return kmeans
+
+
+def test_sample(df, kmeans):
+    df = df.sample(n=2)
+    X = df[FEATURES]
+    predicted_label = kmeans.predict(X)
+    print(predicted_label)
 
 
 if __name__ == '__main__':
-    test()
-
-    new_sample = [[1.2, 3.4, 5.6]]
-
-    # # Use the trained KMeans model to predict the cluster label for the new sample
-    # predicted_label = kmeans.predict(new_sample)
-    #
-    # # Print the predicted cluster label for the new sample
-    # print(predicted_label)
+    ddf = get_data()
+    cln_df = clean_data(ddf)
+    model = run_k_means_clustering(cln_df)
+    test_sample(cln_df, model)
