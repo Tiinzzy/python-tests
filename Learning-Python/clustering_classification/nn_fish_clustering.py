dcb8880b75bd4e783fe41d77ff4cf392ad8ce3b7
Tina <tinzzy.cracked@yahoo.com>
2023-05-10 19:05:28 -0400

    nearest neighbour clustering testing and scaling data

--- Learning-Python/clustering_classification/nn_fish_clustering.py
+++ Learning-Python/clustering_classification/nn_fish_clustering.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import joblib
-from sklearn.cluster import SpectralClustering
+from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn import preprocessing
 
 FEATURES = ['Weight', 'Length1', 'Length2', 'Length3', 'Height', 'Width']
 LABELS = ['Species']
@@ -35,10 +37,15 @@ def draw_scatter(df, dim1, dim2, color_by):
 
 def run_nn_clustering(df, optimized_feature, n_clusters):
     X = df[optimized_feature]
-    model = SpectralClustering(n_clusters=n_clusters, assign_labels='discretize', random_state=0).fit(X)
-    df['label'] = model.labels_
+
+    scaler = preprocessing.StandardScaler().fit(X)
+    X = scaler.transform(X)
+
+    model = NearestNeighbors(n_neighbors=n_clusters, algorithm='ball_tree').fit(X)
+    distances, indices = model.kneighbors(X)
     filename = "Nearest_Neighbour_Fish_Cluster.joblib"
     joblib.dump(model, filename)
+    return model
 
 
 def optimized_features(df):
@@ -48,37 +55,33 @@ def optimized_features(df):
     selector.fit(X, y)
     selected_features = selector.get_support(indices=True)
     features = df.columns[selected_features]
+    print(features)
 
     return features
 
 
-def sample_test(df, op_features):
+def sample_test(df, op_features, clusters):
     df = df.sample(frac=1)
-    df = df.sample(n=50)
+    # df = df.sample(n=len(df))
     X = df[op_features]
 
     filename = "Nearest_Neighbour_Fish_Cluster.joblib"
     loaded_model = joblib.load(filename)
     y_predict = loaded_model.fit_predict(X)
     df['prediction'] = y_predict
-    draw_graph(df, op_features, 'prediction')
+    draw_graph(df, op_features, 'prediction', clusters)
 
 
-def draw_graph(df, features, label_id):
+def draw_graph(df, features, label_id, clusters):
     plt.figure(figsize=(15, 15))
-    plt.xlabel("Length2")
-    plt.ylabel("Length3")
+    plt.xlabel(features[0])
+    plt.ylabel(features[1])
     plt.grid(True)
 
-    plt.scatter(df[df[label_id] == 1][features[0]], df[df[label_id] == 1][features[1]], s=8, c='red',
-                alpha=0.5,
-                label='Type 1', zorder=10)
-    plt.scatter(df[df[label_id] == 0][features[0]], df[df[label_id] == 0][features[1]], s=8, c='gray',
-                alpha=0.5,
-                label='Type 2', zorder=10)
-    # plt.scatter(df[df[label_id] == 2][features[0]], df[df[label_id] == 0][features[1]], s=8, c='blue',
-    #             alpha=0.5,
-    #             label='Type 2', zorder=10)
+    for c in range(clusters):
+        plt.scatter(df[df[label_id] == c][features[0]], df[df[label_id] == c][features[1]], s=8,
+                    alpha=0.5, label='C' + str(c), zorder=10)
+
     plt.legend()
     plt.show()
 
@@ -86,5 +89,6 @@ def draw_graph(df, features, label_id):
 if __name__ == '__main__':
     ddf = get_data()
     optim_feat = optimized_features(ddf)
-    # run_nn_clustering(ddf, optim_feat, 2)
-    sample_test(ddf, optim_feat)
+    optim_feat = ['Height', 'Width']
+    run_nn_clustering(ddf, optim_feat, 6)
+    sample_test(ddf, optim_feat, 6)
