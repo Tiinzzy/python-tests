14078d9e0a4ce3bf4f469fe26e1ed408dbb47819
Tina <tinzzy.cracked@yahoo.com>
2023-05-10 11:49:42 -0400

    still working on fish clustering issue

--- Learning-Python/clustering_classification/nn_fish_clustering.py
+++ Learning-Python/clustering_classification/nn_fish_clustering.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import joblib
 from sklearn.cluster import SpectralClustering
+from sklearn.feature_selection import SelectKBest, f_classif
 
-FEATURES = ['Species', 'Weight', 'Length1', 'Length2', 'Length3', 'Height', 'Width']
+FEATURES = ['Weight', 'Length1', 'Length2', 'Length3', 'Height', 'Width']
+LABELS = ['Species']
 
 
 def get_data():
@@ -30,18 +33,58 @@ def draw_scatter(df, dim1, dim2, color_by):
     plt.show()
 
 
-def run_nn_clustering(df, dim1, dim2, n_clusters):
-    X = df[[dim1, dim2]]
+def run_nn_clustering(df, optimized_feature, n_clusters):
+    X = df[optimized_feature]
     model = SpectralClustering(n_clusters=n_clusters, assign_labels='discretize', random_state=0).fit(X)
     df['label'] = model.labels_
-    return df
+    filename = "Nearest_Neighbour_Fish_Cluster.joblib"
+    joblib.dump(model, filename)
+
+
+def optimized_features(df):
+    X = df[FEATURES]
+    y = df[LABELS]
+    selector = SelectKBest(f_classif, k=2)
+    selector.fit(X, y)
+    selected_features = selector.get_support(indices=True)
+    features = df.columns[selected_features]
+
+    return features
+
+
+def sample_test(df, op_features):
+    df = df.sample(frac=1)
+    df = df.sample(n=50)
+    X = df[op_features]
+
+    filename = "Nearest_Neighbour_Fish_Cluster.joblib"
+    loaded_model = joblib.load(filename)
+    y_predict = loaded_model.fit_predict(X)
+    df['prediction'] = y_predict
+    draw_graph(df, op_features, 'prediction')
+
+
+def draw_graph(df, features, label_id):
+    plt.figure(figsize=(15, 15))
+    plt.xlabel("Length2")
+    plt.ylabel("Length3")
+    plt.grid(True)
+
+    plt.scatter(df[df[label_id] == 1][features[0]], df[df[label_id] == 1][features[1]], s=8, c='red',
+                alpha=0.5,
+                label='Type 1', zorder=10)
+    plt.scatter(df[df[label_id] == 0][features[0]], df[df[label_id] == 0][features[1]], s=8, c='gray',
+                alpha=0.5,
+                label='Type 2', zorder=10)
+    # plt.scatter(df[df[label_id] == 2][features[0]], df[df[label_id] == 0][features[1]], s=8, c='blue',
+    #             alpha=0.5,
+    #             label='Type 2', zorder=10)
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
     ddf = get_data()
-    # show_data_info(ddf)
-    # draw_scatter(ddf, 'Width', 'Height', 'Species')
-    f1 = 'Width'
-    f2 = 'Height'
-    ddf = run_nn_clustering(ddf, f1, f2, 3)
-    draw_scatter(ddf, f1, f2, 'label')
+    optim_feat = optimized_features(ddf)
+    # run_nn_clustering(ddf, optim_feat, 2)
+    sample_test(ddf, optim_feat)
