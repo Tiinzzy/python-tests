c68fc3f3bdf28c318971ac740780307160477aa2
Tina <tina.vatanabadi@gmail.com>
2023-07-04 18:55:00 -0400

    ran test to check hashmap

--- Learning-Python/data-structure/main.py
+++ Learning-Python/data-structure/main.py
@@ -1,27 +1,25 @@
-from bidirect_linkedlist.bidirectional_linkedlist import BidirectionalLinkelist
+# from bidirect_linkedlist.bidirectional_linkedlist import BidirectionalLinkelist
+
+from hash_map.hashmap import HashMap, BidirectionalLinkelist
 
 
 if __name__ == '__main__':
-    bll = BidirectionalLinkelist()
-    bll.add(0)
-    bll.add(1)
-    bll.add(2)
-    bll.add({'a': 1, 'name': 'Tina'}, 3)
-    bll.add(3)
-    bll.add(4)
-    bll.add(5)
-    bll.add(-1, 0)
-    bll.add(300, 3)
-    bll.add(700, 7)
-    bll.add({'a': 1, 'name': 'Tina'})
+    my_hash = HashMap()
 
-    bll.show_all()
-    bll.show_all(reverse=True)
-    print('-----------------------')
+    my_hash.insert('Tina', {1, 2, 3})
+    my_hash.insert('Tania', {4, 5, 6})
+    #
+    my_hash.insert('Kiana', {4, 5, 6})
+    my_hash.insert('Kamran', {1, 2, 3})
+    my_hash.insert('Kevin', {1, 2})
 
-    def my_search(e):
-        return isinstance(e, dict) and e['name'] == 'Tina'
+    my_hash.insert('!', {1, 2, 3})
 
-    print(bll.find_first(my_search))
-    print(bll.find_first(lambda x: isinstance(
-        x, dict) and x['name'] == 'Tina'))
+    my_hash.show_all()
+
+    print('>>>>>>>>>>')
+    my_hash.remove('Kiana')
+    print('>>>>>>>>>>')
+    my_hash.show_all()
+
+
