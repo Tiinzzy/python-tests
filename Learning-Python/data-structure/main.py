9ddcb61ff7946136bfa26a671c4d53c476e3060d
Tina <tina.vatanabadi@gmail.com>
2023-07-11 18:54:18 -0400

    testing tree remove and matrix

--- Learning-Python/data-structure/main.py
+++ Learning-Python/data-structure/main.py
@@ -3,8 +3,10 @@ from tree.organization_chart import OrganizationChart
 from hash_map.hashmap import HashMap, BidirectionalLinkelist
 import string
 import random
+import numpy as np
 import time
 from binary_tree.binary_tree import BinaryTree
+from matrix.matrix import Matrix
 
 letters = string.ascii_lowercase
 
@@ -45,7 +47,7 @@ if __name__ == '__main__':
     # tree.add_sibling(tree.find_first_parent('Kamran'), 'Tina')
     # tree.show_org_chart()
 
-    br_tree = BinaryTree()
+    # br_tree = BinaryTree()
     # br_tree.insert(40)
     # br_tree.insert(39)
     # br_tree.insert(41)
@@ -54,31 +56,52 @@ if __name__ == '__main__':
     # br_tree.show()
     #
     # print(br_tree.node_exist(1.5))
-    print('---------------------------')
+    # print('---------------------------')
+    #
+    # seed = time.time() * 1000
+    # count = 100000
+    # max_num = 10000
+    # search_for = int(random.random() * max_num)
+    #
+    # random.seed(seed)
+    # for i in range(count):
+    #     br_tree.insert(int(random.random() * max_num))
+    #
+    # start_time = time.time()
+    # print(br_tree.node_exist(search_for))
+    # end_time = time.time()
+    # print(end_time - start_time, ' seconds')
+    #
+    # a = []
+    # random.seed(seed)
+    # for i in range(count):
+    #     a.append(int(random.random() * max_num))
+    #
+    # start_time = time.time()
+    # for i in range(len(a)):
+    #     if a[i] == search_for:
+    #         print('FOUND!', a[i], search_for)
+    #         break
+    # end_time = time.time()
+    # print(end_time - start_time, ' seconds')
 
-    seed = time.time() * 1000
-    count = 100000
-    max_num = 10000
-    search_for = int(random.random() * max_num)
 
-    random.seed(seed)
-    for i in range(count):
-        br_tree.insert(int(random.random() * max_num))
+    # br_tree = BinaryTree()
+    # br_tree.insert(40)
+    # br_tree.insert(39)
+    # br_tree.insert(41)
+    # br_tree.insert(38)
+    # br_tree.insert(47)
+    # br_tree.insert(44)
+    # br_tree.show()
+    # br_tree.remove(41)
 
-    start_time = time.time()
-    print(br_tree.node_exist(search_for))
-    end_time = time.time()
-    print(end_time - start_time, ' seconds')
+    print('----------------------')
+    mtr = Matrix()
+    rand_array = np.random.choice([0, 1], size=(4, 4))
+    mtr.random_insert((4, 4), 4)
+    mtr.show_grid()
 
-    a = []
-    random.seed(seed)
-    for i in range(count):
-        a.append(int(random.random() * max_num))
-
-    start_time = time.time()
-    for i in range(len(a)):
-        if a[i] == search_for:
-            print('FOUND!', a[i], search_for)
-            break
-    end_time = time.time()
-    print(end_time - start_time, ' seconds')
+    start = [3, 0]
+    destination = [0, 3]
+    # mtr.path_to_destination(start, destination)
