2f2eb87394548af21ab61ff4699cacf097f5370a
Tina <tina.vatanabadi@gmail.com>
2023-07-17 10:45:48 -0400

    adding start and end to find a path

--- Learning-Python/data-structure/optimized_matrix/main.py
+++ Learning-Python/data-structure/optimized_matrix/main.py
@@ -1,5 +1,12 @@
 from optimized_matrix.matrix import Matrix
+
 if __name__ == '__main__':
+    START = [0, 0];
+    END = [3, 3];
+
     mtr = Matrix()
     grid = mtr.init_grid(4, 4, 3)
     mtr.show_grid()
+    mtr.find_a_path(START, END)
+    mtr.show_grid()
+
