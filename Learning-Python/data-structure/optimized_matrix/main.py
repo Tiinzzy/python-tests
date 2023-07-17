1013c393af82218ca0215264bbe22f1b7914f4f9
Tina <tina.vatanabadi@gmail.com>
2023-07-17 11:26:16 -0400

    fixed the brute force to check for multiple solved solutions

--- Learning-Python/data-structure/optimized_matrix/main.py
+++ Learning-Python/data-structure/optimized_matrix/main.py
@@ -1,12 +1,15 @@
 from optimized_matrix.matrix import Matrix
 
 if __name__ == '__main__':
-    START = [0, 0];
-    END = [3, 3];
+    START = [0, 0]
+    END = [4, 4]
+    ITERATION = []
 
-    mtr = Matrix()
-    grid = mtr.init_grid(4, 4, 3)
-    mtr.show_grid()
-    mtr.find_a_path(START, END)
-    mtr.show_grid()
-
+    for _ in range(500):
+        mtr = Matrix()
+        mtr.init_grid(5, 5, 3)
+        mtr.show_grid()
+        result = mtr.find_a_path(START, END)
+        if result['result']:
+            mtr.show_grid()
+            print(result)
