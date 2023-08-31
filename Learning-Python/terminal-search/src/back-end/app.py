2156a4aa76d481052bca661e2b2f1c439407c3be
Tina <tinzzy.cracked@yahoo.com>
2023-08-31 18:26:51 -0400

    flask api call to send to front-end

--- Learning-Python/terminal-search/src/back-end/app.py
+++ Learning-Python/terminal-search/src/back-end/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import json
 
+from sentiment import Sentiment
+
 app = Flask(__name__)
 
 
@@ -8,6 +10,16 @@ def get_parameters(req):
     return json.loads(req.data.decode('utf8').replace("'", '"'))
 
 
-@app.route('/db/mongodb/connect', methods=["POST"])
-def connect_mongo_db():
-    pass
+@app.route('/search/prompt', methods=["POST"])
+def get_search_prompt():
+    parameters = get_parameters(request)
+    search_prompt = parameters['prompt']
+
+    array_of_titles = Sentiment.find_news_titles(search_prompt)
+
+    titles_sentiment, titles = Sentiment.process_title_sentiment(array_of_titles)
+    prompt_sentiment = Sentiment.process_prompt_sentiment(search_prompt)
+
+    search_prompt = ''
+    return jsonify({'titles_sentiment': titles_sentiment, 'prompt_sentiment': prompt_sentiment,
+                    'array_of_titles': titles})
