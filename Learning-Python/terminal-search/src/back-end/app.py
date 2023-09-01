4037ce50404ed19f5000529d2e13356d0d6adc52
Tina <tinzzy.cracked@yahoo.com>
2023-09-01 16:14:32 -0400

    functions to help with process need to adjust and cleanup app

--- Learning-Python/terminal-search/src/back-end/app.py
+++ Learning-Python/terminal-search/src/back-end/app.py
@@ -42,6 +42,32 @@ def get_all_ten_titles(prompt):
     return all_titles, title_texts
 
 
+def get_separate_ten_titles(prompt):
+    base_url = "https://search.brave.com/news"
+    params = {
+        "q": prompt
+    }
+
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    links = []
+
+    for anchor in soup.find_all("a"):
+        title = anchor.get_text()
+        link = anchor["href"]
+
+        if link.startswith("http"):
+            revised_title = title.replace('\n', ' * ').split(' * ')[-1]
+            links.append({"title": revised_title})
+
+    num_to_save = min(len(links), 10)
+    news_to_save = links[:num_to_save]
+
+    return news_to_save
+
+
 def get_parameters(req):
     return json.loads(req.data.decode('utf8').replace("'", '"'))
 
@@ -50,22 +76,30 @@ def get_parameters(req):
 def get_search_prompt():
     parameters = get_parameters(request)
     search_prompt = parameters['prompt']
+    selected = parameters['msg']
 
-    all_titles_as_text, array_of_titles = get_all_ten_titles(search_prompt)
+    if selected == 'all':
+        # add to another script
+        all_titles_as_text, array_of_titles = get_all_ten_titles(search_prompt)
 
-    nltk_titles_sentiment, nltk_score = NLTKSentiment.process_title_sentiment(all_titles_as_text)
-    nltk_prompt_sentiment = NLTKSentiment.process_prompt_sentiment(search_prompt)
+        nltk_titles_sentiment, nltk_score = NLTKSentiment.process_title_sentiment(all_titles_as_text)
+        nltk_prompt_sentiment = NLTKSentiment.process_prompt_sentiment(search_prompt)
 
-    text_blob_titles_sentiment, text_blob_score = TextBlobSentiment.process_title_sentiment(all_titles_as_text)
-    text_blob_prompt_sentiment = TextBlobSentiment.process_prompt_sentiment(search_prompt)
+        text_blob_titles_sentiment, text_blob_score = TextBlobSentiment.process_title_sentiment(all_titles_as_text)
+        text_blob_prompt_sentiment = TextBlobSentiment.process_prompt_sentiment(search_prompt)
 
-    vader_titles_sentiment, vader_score = VaderSentiment.process_title_sentiment(all_titles_as_text)
-    vader_prompt_sentiment = VaderSentiment.process_prompt_sentiment(search_prompt)
+        vader_titles_sentiment, vader_score = VaderSentiment.process_title_sentiment(all_titles_as_text)
+        vader_prompt_sentiment = VaderSentiment.process_prompt_sentiment(search_prompt)
 
-    search_prompt = ''
-    return jsonify({'nltk_titles_sentiment': nltk_titles_sentiment, 'nltk_prompt_sentiment': nltk_prompt_sentiment,
-                    'nltk_score': nltk_score, 'array_of_titles': array_of_titles,
-                    'text_blob_titles_sentiment': text_blob_titles_sentiment,
-                    'text_blob_prompt_sentiment': text_blob_prompt_sentiment, 'text_blob_score': text_blob_score,
-                    'vader_titles_sentiment': vader_titles_sentiment, 'vader_score': vader_score,
-                    'vader_prompt_sentiment': vader_prompt_sentiment, 'array_of_titles': array_of_titles})
+        return jsonify({'all_ten': True, 'nltk_titles_sentiment': nltk_titles_sentiment,
+                        'nltk_prompt_sentiment': nltk_prompt_sentiment,
+                        'nltk_score': nltk_score, 'array_of_titles': array_of_titles,
+                        'text_blob_titles_sentiment': text_blob_titles_sentiment,
+                        'text_blob_prompt_sentiment': text_blob_prompt_sentiment, 'text_blob_score': text_blob_score,
+                        'vader_titles_sentiment': vader_titles_sentiment, 'vader_score': vader_score,
+                        'vader_prompt_sentiment': vader_prompt_sentiment, 'array_of_titles': array_of_titles})
+    else:
+        array_of_titles = get_separate_ten_titles(search_prompt)
+        nltk_step_sentiment = NLTKSentiment.process_prompt_one_by_one_sentiment(array_of_titles)
+
+        return jsonify({'nltk_step_sentiment': nltk_step_sentiment})
