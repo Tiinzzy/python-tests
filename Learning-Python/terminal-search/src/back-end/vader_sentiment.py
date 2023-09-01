655740fdd3376ee741bf7f0b320c61d16ef77588
Tina <tinzzy.cracked@yahoo.com>
2023-09-01 16:13:57 -0400

    code cleanup

--- Learning-Python/terminal-search/src/back-end/vader_sentiment.py
+++ Learning-Python/terminal-search/src/back-end/vader_sentiment.py
@@ -7,10 +7,8 @@ class VaderSentiment:
     def process_title_sentiment(all_titles):
         sia = SentimentIntensityAnalyzer()
         sentiment_scores = sia.polarity_scores(all_titles)
-        print(sentiment_scores, 'regular?')
 
         compound_score = sentiment_scores['compound']
-        print(compound_score, 'compound')
 
         if compound_score >= 0.05:
             return "Positive", compound_score
@@ -25,7 +23,6 @@ class VaderSentiment:
         sentiment_scores = sia.polarity_scores(prompt)
 
         compound_score = sentiment_scores['compound']
-        print(compound_score, 'search')
         if compound_score >= 0.05:
             return "Positive"
         elif compound_score <= -0.05:
