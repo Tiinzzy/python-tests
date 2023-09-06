e704c3282fb9de09113d5a3dc8c7a70b3fe6a093
Tina <tinzzy.cracked@yahoo.com>
2023-09-06 17:36:06 -0400

    testing gensim google model word2vec

---
+++ Learning-Python/terminal-search/src/back-end/business/classification/gensim_google_w2v.py
@@ -0,0 +1,26 @@
+import gensim.downloader as api
+
+
+def test():
+    wv = api.load('word2vec-google-news-300')
+
+    print(wv['king'])
+    print('-' * 100)
+
+    pairs = [
+        ('car', 'minivan'),  # a minivan is a kind of car
+        ('car', 'bicycle'),  # still a wheeled vehicle
+        ('car', 'airplane'),  # ok, no wheels, but still a vehicle
+        ('car', 'cereal'),  # ... and so on
+        ('car', 'communism'),
+    ]
+    for w1, w2 in pairs:
+        print('%r\t%r\t%.2f' % (w1, w2, wv.similarity(w1, w2)))
+
+    print(wv.most_similar(positive=['king', 'woman'], negative=['man'], topn=1))
+    print(wv.most_similar(positive=['book', 'french'], negative=['english'], topn=1))
+    print(wv.most_similar(positive=['cat', 'dog'], negative=['lion'], topn=1))
+
+
+if __name__ == '__main__':
+    test()
