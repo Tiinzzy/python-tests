20258b5d0174738f53c04458d3e3400a620180bb
Tina <tinzzy.cracked@yahoo.com>
2023-09-07 18:58:22 -0400

    changed classifier and prediction

--- Learning-Python/terminal-search/src/back-end/business/classifier_process.py
+++ Learning-Python/terminal-search/src/back-end/business/classifier_process.py
@@ -3,13 +3,11 @@ from .classification.text_classifier import TextClassifier
 
 
 def get_vector_classification_result(new_text):
-    # print('>>>', os.getcwd())
-    # p = os.getcwd() + '/business/data/recipe_classifier_model'
-    # print(os.path.isfile(p))
-    classifier = TextClassifier('/home/tina/Downloads/food_recipes.csv', 'description', 'category')
-
-    path_to_model_file = os.getcwd() + '/business/data/recipe_classifier_model'
-    classifier.load_model(path_to_model_file)
+    p = os.getcwd() + '/business/data/recipe_classifier_model'
+    classifier = TextClassifier('/home/tina/Downloads/food_recipes.csv', 'description')
+    classifier.train_model()
+    classifier.load_model(p)
     prediction = classifier.predict(new_text)
-    print("Prediction:", prediction)
-    return {'Prediction-result': 'prediction'}
+    print(prediction)
+
+    return {'prediction': prediction.tolist()}
