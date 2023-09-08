79d4470944e9d5e754509a7d49e991facdc90532
Tina <tinzzy.cracked@yahoo.com>
2023-09-08 16:47:15 -0400

    added more function to process vectorizing for a text

--- Learning-Python/terminal-search/src/back-end/business/classifier_process.py
+++ Learning-Python/terminal-search/src/back-end/business/classifier_process.py
@@ -1,13 +1,29 @@
 import os
-from .classification.text_classifier import TextClassifier
+import joblib
+
+configuration = {
+    'models_already_trained': False,
+    'forest_classifier': None,
+    'svm_classifier': None,
+    'adaboost_classifier': None,
+}
 
 
 def get_vector_classification_result(new_text):
-    p = os.getcwd() + '/business/data/recipe_classifier_model'
-    classifier = TextClassifier('/home/tina/Downloads/food_recipes.csv', 'description')
-    classifier.train_model()
-    classifier.load_model(p)
-    prediction = classifier.predict(new_text)
-    print(prediction)
+    if not configuration['models_already_trained']:
+        forest_model_p = os.getcwd() + '/business/data/recipe_classifier_forest_model'
+        svm_model_p = os.getcwd() + '/business/data/recipe_classifier_svm_model'
+        adaboost_model_p = os.getcwd() + '/business/data/recipe_classifier_adaboost_model'
 
-    return {'prediction': prediction.tolist()}
+        configuration['forest_classifier'] = joblib.load(forest_model_p)
+        configuration['svm_classifier'] = joblib.load(svm_model_p)
+        configuration['adaboost_classifier'] = joblib.load(adaboost_model_p)
+
+        configuration['models_already_trained'] = True
+
+    forest_prediction = configuration['forest_classifier'].predict(new_text)
+    svm_prediction = configuration['svm_classifier'].predict(new_text)
+    adaboost_prediction = configuration['adaboost_classifier'].predict(new_text)
+
+    return {'forest_prediction': forest_prediction.tolist(), 'svm_prediction': svm_prediction.tolist(),
+            'adaboost_prediction': adaboost_prediction.tolist()}
