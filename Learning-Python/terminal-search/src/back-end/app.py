ef72616bf07e30a14867a33163d0b23166fba445
Tina <tinzzy.cracked@yahoo.com>
2023-09-18 17:52:08 -0400

    added processing function to load the model and give the prediction

--- Learning-Python/terminal-search/src/back-end/app.py
+++ Learning-Python/terminal-search/src/back-end/app.py
@@ -4,6 +4,7 @@ import json
 from business.total_process import get_all_ten_titles, process_sentiment_for_all
 from business.separate_process import get_separate_ten_titles, process_sentiment_separately
 from business.process_classification import process_tex_classification
+from business.process_prediction import prcess_text_prediction
 
 app = Flask(__name__)
 
@@ -44,6 +45,7 @@ def get_classifying_text():
 def get_text_prediction():
     parameters = get_parameters(request)
     text = parameters['text']
-    print(text)
+
+    result = prcess_text_prediction(text)
 
     return jsonify({'result': True})
