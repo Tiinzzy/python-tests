4397b0eac7d79b5434e21b2397510a9d42283de5
Tina <tinzzy.cracked@yahoo.com>
2023-10-17 08:38:27 -0400

    created a django backend

---
+++ Learning-Python/django-app/backend/backend/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from todo import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
