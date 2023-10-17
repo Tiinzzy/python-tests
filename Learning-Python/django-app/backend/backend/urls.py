f9180632c0fd71fb1c5d3b6a82a6f2cf2f6a0b5d
Tina <tinzzy.cracked@yahoo.com>
2023-10-17 16:48:14 -0400

    added more api to test

--- Learning-Python/django-app/backend/backend/urls.py
+++ Learning-Python/django-app/backend/backend/urls.py
@@ -3,15 +3,13 @@ from django.urls import path, include
 from rest_framework import routers
 from todo import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
-
-
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/sample/', views.sample_api, name='sample_api'),
+    path('api/post/', views.post_data, name='post_data'),
+    path('csrf-token/', views.get_csrf_token, name='get_csrf_token'),
 ]
