a2d055e8c7805549a05d1b7456b836a5bc820a7b
Tina <tinzzy.cracked@yahoo.com>
2023-10-19 10:46:06 -0400

    created a sub html template to include in some templates and do cllabacks

--- Learning-Python/learning-django/monthly_challenges/challenges/urls.py
+++ Learning-Python/learning-django/monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path("january", views.january), regular one by one path setting but down is dynamic and get anything
-    path("", views.index), # path for /challenges/ will be triggered
+    path("", views.index, name="index"), # path for /challenges/ will be triggered
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
