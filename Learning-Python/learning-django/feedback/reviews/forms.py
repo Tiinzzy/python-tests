556acc4a82c4e4e4e0e0dd79d6bcb8fc09d4004a
Tina <tinzzy.cracked@yahoo.com>
2023-10-27 15:05:23 -0400

    prevented repetition in the html for the forms

--- Learning-Python/learning-django/feedback/reviews/forms.py
+++ Learning-Python/learning-django/feedback/reviews/forms.py
@@ -6,3 +6,5 @@ class ReviwForm(forms.Form):
         "required": "Your name must not be empty!",
         "max_length": "PLease enter a shorter name!"
     })
+    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
