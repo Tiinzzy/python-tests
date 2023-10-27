7082551cfc6a25393fa8e469437b3da78773f960
Tina <tinzzy.cracked@yahoo.com>
2023-10-27 15:54:06 -0400

    created a class based view to replace with function based view.
    entered data into database and fixed model and forms

--- Learning-Python/learning-django/feedback/reviews/forms.py
+++ Learning-Python/learning-django/feedback/reviews/forms.py
@@ -1,10 +1,28 @@
 from django import forms
+from .models import Reviw
+
+# class ReviwForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+#         "required": "Your name must not be empty!",
+#         "max_length": "PLease enter a shorter name!"
+#     })
+#     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
 
 
-class ReviwForm(forms.Form):
-    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
-        "required": "Your name must not be empty!",
-        "max_length": "PLease enter a shorter name!"
-    })
-    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+class ReviwForm(forms.ModelForm):
+    class Meta:
+        model = Reviw
+        fields = '__all__'
+        # exclude = ['owner_comment']
+        labels = {
+            "user_name": "Your Name",
+            "review_text": 'Your Feedback',
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required" : "Your name must not be empty!",
+                "max_length": "PLease enter a shorter name!"
+            }
+        }
--- Learning-Python/learning-django/feedback/reviews/models.py
+++ Learning-Python/learning-django/feedback/reviews/models.py
@@ -7,3 +7,4 @@ class Reviw(models.Model):
     user_name = models.CharField(max_length=100)
     review_text = models.TextField()
     rating = models.IntegerField()
+    # owner_comment = models.TextField()
