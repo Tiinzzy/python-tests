dec4ceadb86db5dc2942e755ee7ce55ede72602c
Tina <tinzzy.cracked@yahoo.com>
2023-10-27 09:20:52 -0400

    fixed admin page for display and filters

--- Learning-Python/learning-django/my_site/blog/admin.py
+++ Learning-Python/learning-django/my_site/blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Post, Author, Tag
+
 # Register your models here.
 
-admin.site.register(Post)
+
+class postAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, postAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
