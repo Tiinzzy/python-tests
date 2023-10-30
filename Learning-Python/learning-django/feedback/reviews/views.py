55ee65c91d52d867981fe39590ec80305ffaaba4
Tina <tinzzy.cracked@yahoo.com>
2023-10-30 13:38:59 -0400

    transformed into detailed view instead of template view

--- Learning-Python/learning-django/feedback/reviews/views.py
+++ Learning-Python/learning-django/feedback/reviews/views.py
@@ -4,23 +4,23 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from .forms import ReviwForm
-from .models import Reviw
+from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
 
 class ReviewView(View):
     def get(self, request):
-        form = ReviwForm()
+        form = ReviewForm()
         return render(request, "reviews/review.html", {
             "form": form
         })
 
     def post(self, request):
-        form = ReviwForm(request.POST)
+        form = ReviewForm(request.POST)
 
         if form.is_valid():
             form.save()
@@ -42,7 +42,7 @@ class ThanYouView(TemplateView):
 
 class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
-    model = Reviw
+    model = Review
     context_object_name = "reviews"
 
     # to only get a sample of quesries with a filter
@@ -51,15 +51,19 @@ class ReviewsListView(ListView):
     #     data = base_query.filter(rating__gt=4)
     #     return data
 
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
+    model = Review
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        selected_review = Reviw.objects.get(pk=review_id)
-        context["review"] = selected_review
-        return context
+
+
+    # only if it is templateview extension
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     review_id = kwargs["id"]
+    #     selected_review = Reviw.objects.get(pk=review_id)
+    #     context["review"] = selected_review
+    #     return context
 
 # def review(request):
 #     # if the form is html and made with htm
