9e0d1ad17c9a5df6c92c2ecb36df061eb1cb7325
Tina <tinzzy.cracked@yahoo.com>
2023-11-01 13:28:47 -0400

    wrote test properly for genre dao class methods

--- Learning-Python/netflix-model/tests/test_genre_dao/test_genre_dao.py
+++ Learning-Python/netflix-model/tests/test_genre_dao/test_genre_dao.py
@@ -1,20 +1,35 @@
 import unittest
-
-import os
-import sys
-
-project_root = os.path.dirname(os.path.realpath("__file__"))
-project_root = project_root.replace("/tests/test_genre_dao", "/model/genre")
-sys.path.append(project_root)
-
-import genre_dao
+from model.genre.genre_dao import GenreDao
 
 
 class TestGenreDao(unittest.TestCase):
 
     def test_save_to_table(self):
-        gnr = genre_dao.GenreDao(description="Test Genre")
+        gnr = GenreDao(description="Test Genre")
         gnr.save_to_table()
 
         self.assertTrue(gnr.get_oid() > 0)
         self.assertEqual(gnr.get_description(), "Test Genre")
+
+
+    def test_load_all(self):
+        gnr = GenreDao.load_all()
+        print(gnr)
+        self.assertTrue(len(gnr) > 0)
+    #
+    def test_add_and_delete(self):
+        gnr = GenreDao()
+        gnr.set_description("Test1")
+        gnr.save_to_table()
+        self.assertEqual(gnr.get_description(), "Test1")
+
+        new_oid = gnr.get_oid()
+        result = gnr.delete_genre(new_oid)
+        self.assertEqual(result, None)
+
+
+    def test_update(self):
+        gnr = GenreDao(oid=56)
+        gnr.set_description("Musical")
+        gnr.save_to_table()
+        self.assertEqual(gnr.get_description(), "Musical")
