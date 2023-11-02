a80333a4eed1111671f8e3eacbf8cc0dda300679
Tina <tinzzy.cracked@yahoo.com>
2023-11-02 12:45:31 -0400

    initialized a class for test and package

---
+++ Learning-Python/netflix-model/tests/test_tv_series_dao/test_tv_series_dao.py
@@ -0,0 +1,10 @@
+import unittest
+from model.tvseries.tv_series_dao import TvSeriesDao
+from model.tvseries.seasons_dao import SeasonDao
+from model.tvseries.episodes_dao import EpisodeDao
+
+
+class TestTvSeriesDao(unittest.TestCase):
+
+    def test_save_to_table(self):
+        pass
