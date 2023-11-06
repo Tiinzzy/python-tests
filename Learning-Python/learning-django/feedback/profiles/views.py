d7f4d9a3559b3a8db2e2b5c274a9e487841bca73
Tina <tina.vatanabadi@gmail.com>
2023-11-06 08:33:05 -0500

    class to create a vie for profiles url using the create view and our model

--- Learning-Python/learning-django/feedback/profiles/views.py
+++ Learning-Python/learning-django/feedback/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -12,22 +13,30 @@ from .models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
+# since using the class bellow can delete the forms.py 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url ="/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+# can use class on the top instead of the bottom 
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
 
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
 
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
