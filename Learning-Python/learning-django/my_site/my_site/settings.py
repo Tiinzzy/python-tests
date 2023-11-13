b1a5b50de53ca571381e2ccdbe6fbb20df7bc92a
Tina <tina.vatanabadi@gmail.com>
2023-11-13 10:12:11 -0500

    changed settings to make it ready for deployment

--- Learning-Python/learning-django/my_site/my_site/settings.py
+++ Learning-Python/learning-django/my_site/my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o+%$(m_%7a^$&--@=ok3d^1_gfs!5=378_oc&@)pqv87hc!pk2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = "my_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [ BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIR = [ BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIR = [BASE_DIR / "static"]
 
 
 # Default primary key field type
